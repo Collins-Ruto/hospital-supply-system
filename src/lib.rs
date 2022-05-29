de62a0f5a07fbf14257a9c99e262c6984032ba97
collins-ruto <collinsruto48@gmail.com>
2022-05-29 15:19:34 +0300

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{env, near_bindgen};
+use near_sdk::collections::UnorderedMap;
+
+near_sdk::setup_alloc!();
+
+// 1. Main Struct
+
+// 2. Default Implementation
+
+// 3. Core Logic
+
+// 4. Testsn
