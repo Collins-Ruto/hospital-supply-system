3bea2fdb91b7c03f03a665dbf079281f3a5f0f11
collins <collins@localhost.localdomain>
2022-05-30 16:32:37 +0300

    Initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,21 @@ use near_sdk::collections::UnorderedMap;
 
 near_sdk::setup_alloc!();
 
+#[near_bindgen]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+
 // 1. Main Struct
-
-pub fn Hospitals {
-
+pub Struct Hospitals {
+    hospital_name: String,
+    hospital_level: String,
+    hospital_county: String,
 }
 
+impl Hospitals {
+    pub fn new(&mut self, name: String) {
+        println!("{}", name);
+    }
+}
 // 2. Default Implementation
 
 // 3. Core Logic
