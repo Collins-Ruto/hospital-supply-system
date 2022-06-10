9a9f78c746fe1cb51d6bf195f4a10130aa07462e
collins-ruto <collinsruto48@gmail.com>
2022-06-10 23:23:08 +0300

    initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,91 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen};
-use near_sdk::collections::UnorderedMap;
-
-near_sdk::setup_alloc!();
+use near_sdk::near_bindgen;
+use std::collections::HashMap;
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
-
-// 1. Main Struct
-pub Struct Hospitals {
-    hospital_name: String,
-    hospital_level: String,
-    hospital_county: String,
+pub struct Supplies {
+    data: HashMap<String, Supply>,
+    hospitals: HashMap<String, Hospitals>,
+    suppliers: HashMap<String, Supplier>,
+    items: HashMap<String, Item>,
+    ids: i32,
 }
 
-impl Hospitals {
-    pub fn new(&mut self, name: String) {
-        println!("{}", name);
+#[near_bindgen]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+pub struct Supply {
+    hospital: Hospitals,
+    supplier: Supplier,
+    supplies: HashMap<String, Item>,
+    sponsor: String,
+    fund: f32,
+    misc: HashMap<String, f32>,
+}
+
+#[near_bindgen]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+pub struct Hospitals {
+    hospital_level: String,
+    hospital_county: String,
+    supplies: HashMap<String, Item>,
+}
+
+#[near_bindgen]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+pub struct Item {
+    cost: f32,
+    manufacturer: String,
+    types: String,
+    date_of_manufacture: String,
+}
+
+#[near_bindgen]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+pub struct Supplier {
+    types: String,
+    supply_runs: u32,
+    supply_worth: f32,
+}
+
+#[near_bindgen]
+impl Supplies {
+    #[private]
+    pub fn add_hospital(&mut self, name: String, level: String, county: String) {
+        let new_hospital = Hospitals {
+            hospital_level: level,
+            hospital_county: county,
+            supplies: HashMap::new(),
+        };
+        self.hospitals.insert(name, new_hospital);
+    }
+
+    #[private]
+    pub fn add_item(
+        &mut self,
+        name: String,
+        cost: f32,
+        manufacturer: String,
+        types: String,
+        date: String,
+    ) {
+        let new_item = Item {
+            cost: cost,
+            manufacturer: manufacturer,
+            types: types,
+            date_of_manufacture: date,
+        };
+        self.items.insert(name, new_item);
+    }
+
+    #[private]
+    pub fn add_supplier(&mut self, name: String, types: String) {
+        let new_supplier = Supplier {
+            types: types,
+            supply_runs: 0,
+            supply_worth: 0.0,
+        };
+        self.suppliers.insert(name, new_supplier);
     }
 }
 // 2. Default Implementation
